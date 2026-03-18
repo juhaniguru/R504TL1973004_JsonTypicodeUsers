99d8360a370e5033936fe30b48e3ef59f5634f8c
juhani.kuru <juhanialeksikuru@gmail.com>
2026-03-18 19:07:13 +0200

    - implement ui

--- app/src/main/java/com/example/r504tl1973004_jsontypicodeusers/MainActivity.kt
+++ app/src/main/java/com/example/r504tl1973004_jsontypicodeusers/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.r504tl1973004_jsontypicodeusers.presentation.UsersScreenRoot
 import com.example.r504tl1973004_jsontypicodeusers.ui.theme.R504TL1973004_JsonTypicodeUsersTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             R504TL1973004_JsonTypicodeUsersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                UsersScreenRoot()
             }
         }
     }
--- app/src/main/java/com/example/r504tl1973004_jsontypicodeusers/presentation/UsersScreen.kt
+++ app/src/main/java/com/example/r504tl1973004_jsontypicodeusers/presentation/UsersScreen.kt
@@ -1,10 +1,24 @@
 package com.example.r504tl1973004_jsontypicodeusers.presentation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.r504tl1973004_jsontypicodeusers.R
 
 @Composable
 fun UsersScreenRoot(modifier: Modifier = Modifier) {
@@ -14,12 +28,36 @@ fun UsersScreenRoot(modifier: Modifier = Modifier) {
     UsersScreen(state = state)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UsersScreen(modifier: Modifier = Modifier, state: UsersState) {
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text(stringResource(R.string.users))
+        })
+    }) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues), contentAlignment = Alignment.Center
+        ) {
+            when {
+                state.loading -> CircularProgressIndicator()
+                state.error != null -> Text(state.error)
+                else -> UsersList(users = state.items)
+            }
+        }
 
+    }
 }
 
 @Composable
-fun UsersList(modifier: Modifier = Modifier) {
-
+fun UsersList(modifier: Modifier = Modifier, users: List<User>) {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+        items(items = users, key = { user -> user.id }) { user ->
+            Card(modifier = Modifier.fillParentMaxWidth()) {
+                Text(user.email)
+            }
+        }
+    }
 }
